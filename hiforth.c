ff2171f49efacba853d4193adbd78c0b52f11506
Stanislav Paskalev <spaskalev@protonmail.com>
2024-03-23 22:53:13 +0200

    use only pointers in the ctx, not offsets

--- hiforth.c
+++ hiforth.c
@@ -1,19 +1,22 @@
 #include <setjmp.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 
+#include <stdio.h>
+
+#define WORD_SIZE sizeof(intptr_t)
+
 struct ctx;
 
-typedef void (*machine_word)(struct ctx* c);
+typedef void (*word)(struct ctx* c);
 
 struct ctx {
-   unsigned char    *memory;
-   long int          ip;           // instruction pointer
-
-   unsigned char    *return_stack;
-   long int          rsi;          // return stack index
-
-   jmp_buf      trampoline;
+   unsigned char *ip;           // instruction pointer
+   unsigned char *dsi;          // data stack index
+   unsigned char *rsi;          // return stack index
+   intptr_t      *w;            // work register
+   jmp_buf        trampoline;
 };
 
 /*
@@ -24,10 +27,8 @@ void trampoline(struct ctx* c) {
       return;
    }
    while(1) {
-      intptr_t *f;
-      memcpy(&f, c->memory + c->ip, sizeof(f));
-
-      ((machine_word) *f)(c);
+      memcpy(&c->w, c->ip, sizeof(c->w));   // Store first indirection in work register
+      ((word) *(c->w))(c);                  // Call second indirection
    }
 }
 
@@ -35,30 +36,36 @@ void trampoline(struct ctx* c) {
  * Stops the execution of the provided context.
  */
 void halt(struct ctx* c) {
+   printf("halt reached!\n");
    longjmp(c->trampoline, 1);
 }
 
 /*
- * Handler for non-machine words
+ * Starting handler for non-machine words.
  */
 void enter(struct ctx* c) {
-   c->return_stack[c->rsi++] = c->ip;  // save current ip
-   c->ip++;                            // calculate new ip
+   c->rsi += WORD_SIZE;                 // Bump the return stack
+   memcpy(c->rsi, &c->ip, WORD_SIZE);   // Store the current ip on it
+
+   c->w += WORD_SIZE;                   // Bump the work register
+   memcpy(c->ip, &c->w, WORD_SIZE);     // Update the ip with the next word
 }
 
 int main() {
    unsigned char memory[4096] = {0};
+   unsigned char data_stack[128] = {0};
    unsigned char return_stack[128] = {0};
 
-   machine_word p = &halt;
-   memcpy(memory + 8, &p, sizeof(machine_word));
+   word p = &halt;
+   memcpy(memory + 8, &p, sizeof(word));
 
    intptr_t *i = (intptr_t*) (memory+8);
    memcpy(memory, &i, sizeof(intptr_t));
 
    struct ctx c = {0};
-   c.memory = memory;
-   c.return_stack = return_stack;
+   c.ip = memory;
+   c.dsi = data_stack;
+   c.rsi = return_stack;
 
    trampoline(&c);
 }
