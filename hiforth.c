d972b8d2e8e9eec77727007faa513e7ebef72f8d
Stanislav Paskalev <spaskalev@protonmail.com>
2024-03-23 21:50:34 +0200

    got ITC working via trampoline

--- hiforth.c
+++ hiforth.c
@@ -1,30 +1,41 @@
+#include <setjmp.h>
+#include <stdint.h>
+#include <string.h>
+
 struct ctx;
 
 typedef void (*machine_word)(struct ctx* c);
 
 struct ctx {
-   int         *memory;
-   int          ip;           // instruction pointer
+   unsigned char    *memory;
+   long int          ip;           // instruction pointer
 
-   int         *return_stack;
-   int          rsi;          // return stack index
+   unsigned char    *return_stack;
+   long int          rsi;          // return stack index
 
-   int          is_running;
-   machine_word next;
+   jmp_buf      trampoline;
 };
 
 /*
  * Trampoline executor to avoid using the native stack.
  */
 void trampoline(struct ctx* c) {
-   while(c->is_running) c->next(c);
+   if (setjmp(c->trampoline)) {
+      return;
+   }
+   while(1) {
+      intptr_t *f;
+      memcpy(&f, c->memory + c->ip, sizeof(f));
+
+      ((machine_word) *f)(c);
+   }
 }
 
 /*
  * Stops the execution of the provided context.
  */
 void halt(struct ctx* c) {
-   c->is_running = 0;
+   longjmp(c->trampoline, 1);
 }
 
 /*
@@ -36,10 +47,18 @@ void enter(struct ctx* c) {
 }
 
 int main() {
-   int memory[4096] = {0};
-   int retstack[128] = {0};
+   unsigned char memory[4096] = {0};
+   unsigned char return_stack[128] = {0};
 
-   struct ctx c = {memory, 0, retstack, 0, 1, &halt};
+   machine_word p = &halt;
+   memcpy(memory + 8, &p, sizeof(machine_word));
+
+   intptr_t *i = (intptr_t*) (memory+8);
+   memcpy(memory, &i, sizeof(intptr_t));
+
+   struct ctx c = {0};
+   c.memory = memory;
+   c.return_stack = return_stack;
 
    trampoline(&c);
 }
